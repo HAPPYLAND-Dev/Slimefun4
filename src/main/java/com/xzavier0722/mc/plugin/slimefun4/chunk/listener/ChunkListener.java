063146262bdcc596e445e9a1aca9440f484e2f21
Daniel Zhang <xiaozhangshiw@163.com>
2023-08-14 10:22:44 +0800

    区块加载缓冲器

--- src/main/java/com/xzavier0722/mc/plugin/slimefun4/chunk/listener/ChunkListener.java
+++ src/main/java/com/xzavier0722/mc/plugin/slimefun4/chunk/listener/ChunkListener.java
@@ -1,7 +1,10 @@
 package com.xzavier0722.mc.plugin.slimefun4.chunk.listener;
 
+import com.xzavier0722.mc.plugin.slimefun4.storage.controller.BlockDataController;
 import io.github.thebusybiscuit.slimefun4.implementation.Slimefun;
 import org.bukkit.Bukkit;
+import org.bukkit.Chunk;
+import org.bukkit.World;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.world.ChunkLoadEvent;
@@ -9,12 +12,32 @@ import org.bukkit.event.world.WorldLoadEvent;
 import org.bukkit.event.world.WorldUnloadEvent;
 import org.bukkit.scheduler.BukkitRunnable;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class ChunkListener implements Listener {
+
+    private static final ConcurrentHashMap<World, HashMap<Chunk, Boolean>> delayLoad = new ConcurrentHashMap<>();
 
     @EventHandler
     public void onChunkLoad(ChunkLoadEvent e) {
-        Slimefun.getDatabaseManager().getBlockDataController(e.getWorld()).loadChunk(e.getChunk(), e.isNewChunk());
+        BlockDataController controller = Slimefun.getDatabaseManager().getBlockDataController(e.getWorld());
+        World world = e.getWorld();
+
+        if (controller == null) {
+            delayLoad.computeIfAbsent(world, k -> new HashMap<>()).put(e.getChunk(), e.isNewChunk());
+        } else {
+            controller.loadChunk(e.getChunk(), e.isNewChunk());
+            Map<Chunk, Boolean> list = delayLoad.remove(world);
+
+            if (list != null) {
+                list.forEach(controller::loadChunk);
+            }
+        }
     }
+
 
     @EventHandler
     public void onWorldLoad(WorldLoadEvent e) {
@@ -33,6 +56,7 @@ public class ChunkListener implements Listener {
             public void run() {
                 if (Bukkit.getServer().isStopping()) return;
                 Slimefun.getDatabaseManager().unloadWorld(e.getWorld(), true);
+                delayLoad.remove(e.getWorld());
             }
         }.runTaskAsynchronously(Slimefun.instance());
     }
